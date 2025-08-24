645824c6feba9eb9b9014ebc0aa2f582517b755e
Yudai Murai <baby.o.chameleon@gmail.com>
2025-08-24 18:12:50 +0900

    Refactoring

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,26 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-import { Authenticator } from '@aws-amplify/ui-react'; 
-// Amplify UI のデフォルトCSSもインポート
-import '@aws-amplify/ui-react/styles.css'; 
-
-// Amplify の設定
 Amplify.configure(awsExports);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    {/* ★ Authenticator.Provider で App コンポーネントをラップする */}
     <Authenticator.Provider>
       <App />
     </Authenticator.Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
